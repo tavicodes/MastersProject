23e0fe76e56df66581556fab0cf559816b3f197f
Octavia L <octavial@gmx.com>
2023-08-01 13:11:29 +0100

    Program to auto increment JSON plot story values.

---
+++ IncJSON/Program.cs
@@ -0,0 +1,18 @@
+﻿var lines = File.ReadLines("input.txt");
+char[] separators = new char[] { ' ', ',' };
+List<String> outputLines = new List<String>();
+
+foreach (var line in lines)
+{
+    String newLine = line;
+    if (line.StartsWith("\"id\":") || line.StartsWith("\"parent\":") || line.StartsWith("\"allow\":") || line.StartsWith("\"deny\":"))
+    {
+        String[] subs = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        if (Int32.Parse(subs[1]) != 0) subs[1] = (Int32.Parse(subs[1]) + 1).ToString();
+        newLine = String.Concat(subs[0], ' ', subs[1]);
+        if (!line.StartsWith("\"deny:\"")) newLine += ',';
+    }
+    outputLines.Add(newLine);
+}
+
+File.WriteAllLines("output.txt", outputLines.ToArray());
